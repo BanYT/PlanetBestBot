40f858baa1f2ea9a0352f964d07e913f6431faaa
BanYT <44928120+BanYT@users.noreply.github.com>
2019-04-20 09:51:24 -0400

    Update cmdloader.js

--- util/cmdloader.js
+++ util/cmdloader.js
@@ -5,44 +5,20 @@ const readdir = promisify(require("fs").readdir);
 module.exports = async (client) => {
     client.commands = new Discord.Collection();
     client.aliases = new Discord.Collection();
-    client.informationCommands = new Discord.Collection();
-    client.miscCommands = new Discord.Collection();
-    client.moderationCommands = new Discord.Collection();
-    client.funCommands = new Discord.Collection();
-
-const infoFiles = await readdir("./commands/Information/");
-const miscFiles = await readdir("./commands/Misc/");
-const modFiles = await readdir("./commands/Moderation/");
-const funFiles = await readdir("./commands/Fun/");
+    client.cmdsCommands = new Discord.Collection();
 
 
-funFiles.forEach(f => {
-        if (!f.endsWith(".js")) return;
-        let fun = require(`../commands/Fun/${f}`);
-        client.funCommands.set(fun.help.name, fun);
-        client.commands.set(fun.help.name, fun);
-        client.aliases.set(fun.help.alias, fun);
-        });
-infoFiles.forEach(f => {
+const cmdsFiles = await readdir("./commands/");
+
+
+
+
+cmdsFiles.forEach(f => {
     if (!f.endsWith(".js")) return;
-		let info = require(`../commands/Information/${f}`);
-    client.informationCommands.set(info.help.name, info);
-    client.commands.set(info.help.name, info);
-	client.aliases.set(info.help.alias, info);
-	});
-miscFiles.forEach(f => {
-    if (!f.endsWith(".js")) return;
-		let misc = require(`../commands/Misc/${f}`);
-    client.miscCommands.set(misc.help.name, misc);
-    client.commands.set(misc.help.name, misc);
-	client.aliases.set(misc.help.alias, misc);
-	});
-modFiles.forEach(f => {
-    if (!f.endsWith(".js")) return;
-		let mod = require(`../commands/Moderation/${f}`);
-    client.moderationCommands.set(mod.help.name, mod);
-    client.commands.set(mod.help.name, mod);
-	client.aliases.set(mod.help.alias, mod);
+		let cmds = require(`../commands/${f}`);
+    client.moderationCommands.set(cmds.help.name, cmds);
+    client.commands.set(cmds.help.name, cmds);
+	client.aliases.set(cmds.help.alias, cmds);
 	});
 console.log(`loaded ${client.commands.size} commands`);
 console.log(`loaded ${client.aliases.size} aliases`);
